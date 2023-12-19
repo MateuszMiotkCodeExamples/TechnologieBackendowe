723ba8d3986a6fa952f0c58cd3c90c583a0f17ef
Mateusz Miotk <mmiotk@inf.ug.edu.pl>
2023-12-19 12:52:05 +0100

    Add package.json to form-handling project

--- form-handling/handlers.js
+++ form-handling/handlers.js
@@ -1,3 +1,19 @@
+//handlers.js
+const pathUtils = require('path')
+const fs = require('fs')
+// create directory to store vacation photos (if it doesn't already exist)
+const dataDir = pathUtils.resolve(__dirname, '..', 'data')
+const vacationPhotosDir = pathUtils.join(dataDir, 'vacation-photos')
+if(!fs.existsSync(dataDir)) fs.mkdirSync(dataDir)
+if(!fs.existsSync(vacationPhotosDir)) fs.mkdirSync(vacationPhotosDir)
+function saveContestEntry(contestName, email, year, month, photoPath) {
+// TODO...this will come later
+}
+// we'll want these promise-based versions of fs functions later
+const { promisify } = require('util')
+const mkdir = promisify(fs.mkdir)
+const rename = promisify(fs.rename)
+
 exports.newsletterSignup = (req, res) => {
     res.render('newsletter-signup', { csrf: 'CSRF token goes here' })
 }
@@ -39,8 +55,13 @@ exports.vacationPhotoContestProcess = (req, res, fields, files) => {
     res.redirect(303, '/vacation-photo-thank-you')
 }
 
-exports.api.vacationPhotoContest = (req, res, fields, files) => {
-    console.log('field data: ', fields)
-    console.log('files: ', files)
+exports.api.vacationPhotoContest = async (req, res, fields, files) => {
+    const photo = files.photo[0]
+    const dir = vacationPhotosDir + '/' + Date.now()
+    const path = dir + '/' + photo.originalFilename
+    await mkdir(dir)
+    await rename(photo.path, path)
+    saveContestEntry('vacation-photo', fields.email,
+        req.params.year, req.params.month, path)
     res.send({ result: 'success' })
 }
